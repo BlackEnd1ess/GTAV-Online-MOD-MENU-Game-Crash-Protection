15b3b92b34c1565aea4640a00103d4c958e91497
BlackEndless <127990034+BlackEnd1ess@users.noreply.github.com>
2026-06-08 20:41:37 +0200

    Update test_cap_len.py

--- test_cap_len.py
+++ test_cap_len.py
@@ -1,15 +1,9 @@
 from collections import Counter
 import json,time,pydivert
 
-FILTER="outbound and udp and udp.DstPort == 61456"
-def msg_type(payload: bytes) -> str:
-	if len(payload) >= 2:
-		return payload[:2].hex()
-	return "short"
-
-with pydivert.WinDivert(FILTER) as w:
+with pydivert.WinDivert("outbound and udp and udp.DstPort == 61456") as w:
 	for pkt in w:
 		payload=pkt.payload or b""
-		if len(payload) > 700:
+		if len(payload) > 750:
 			continue
 		w.send(pkt)
