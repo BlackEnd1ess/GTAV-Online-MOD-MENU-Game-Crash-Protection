f3d1dc33f1ef6e705c18ca05128785a9d0841e8d
BlackEndless <127990034+BlackEnd1ess@users.noreply.github.com>
2026-06-16 22:01:34 +0200

    Delete test_cap_len.py

--- test_cap_len.py
+++
@@ -1,9 +0,0 @@
-from collections import Counter
-import json,time,pydivert
-
-with pydivert.WinDivert("outbound and udp and udp.DstPort == 61456") as w:
-	for pkt in w:
-		payload=pkt.payload or b""
-		if len(payload) > 750:
-			continue
-		w.send(pkt)
