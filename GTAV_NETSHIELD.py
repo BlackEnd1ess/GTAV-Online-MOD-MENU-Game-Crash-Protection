e4759106e1a9719c9dada39b99e08877ad8f69f1
BlackEndless <127990034+BlackEnd1ess@users.noreply.github.com>
2026-06-17 01:55:56 +0200

    Update GTAV_NETSHIELD.py

--- GTAV_NETSHIELD.py
+++ GTAV_NETSHIELD.py
@@ -1,23 +1,37 @@
 ##info: block crash packets by ingame cheaters/modders
-## can cause connection lost in 
-import pydivert
+## can cause connection lost in session
+from datetime import datetime
+import pydivert,time,os,gc
+gc.enable()
 
 RELAY={"185.56.65.167","185.56.65.168","185.56.65.169","185.56.65.170","185.56.65.171","185.56.65.172"}
+
+start=time.time()
+
+SUSPECT_OUT_LEN=875
+SUSPECT_IN_LEN=208
+OUTPUT_LOG=True
+
 FILTER=("udp and ("
 	"(outbound and udp.DstPort == 61456) or "
 	"(inbound and udp.SrcPort == 61456 and udp.DstPort == 6672)"
 	")")
 
-print('GTAV Online Net-Shield')
+os.system('cls')
+print("-- GTAV Online Net-Shield v1.1 --")
 with pydivert.WinDivert(FILTER) as w:
 	for pkt in w:
-		payload = pkt.payload or b""
-		plen = len(payload)
-		src = str(pkt.src_addr)
-		dst = str(pkt.dst_addr)
-		if pkt.is_outbound and pkt.dst_port == 61456:
-			if plen == 875:
-				continue
-		if (pkt.is_inbound and src in RELAY and pkt.src_port == 61456 and pkt.dst_port == 6672 and plen == 208):
+		payload=pkt.payload or b""
+		plen=len(payload)
+		src=str(pkt.src_addr)
+		dst=str(pkt.dst_addr)
+		ddt=datetime.now().strftime("%H:%M:%S")
+		if pkt.is_outbound and pkt.dst_port == 61456 and plen == SUSPECT_OUT_LEN:
+			if OUTPUT_LOG:
+				print(f'[INFO] <{ddt}> blocked OUTBOUND crash-packet ::: paket_details: IP_ADDR {src}:{pkt.src_port} -> {dst}:{pkt.dst_port} ::: data={payload[:8].hex()}')
+			continue
+		if (pkt.is_inbound and src in RELAY and pkt.src_port == 61456 and pkt.dst_port == 6672 and plen == SUSPECT_IN_LEN):
+			if OUTPUT_LOG:
+				print(f'[INFO] <{ddt}> blocked INBOUND crash-packet ::: paket_details: IP_ADDR {src}:{pkt.src_port} -> {dst}:{pkt.dst_port} ::: data={payload[:8].hex()}')
 			continue
 		w.send(pkt)
